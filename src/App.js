4937b83d2a44d549ef81a21254f80511ee7f584c
unknown <123dragaoo@gmail.com>
2022-04-27 09:10:59 -0300

    rota repos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 import Search from './components/Home/Search';
 import axios from './services/axios'
+import { Link } from 'react-router-dom'
 
-const searchUser = async (user) => {
-  console.log(user)
-  const result = await axios.getAll(user)
-  console.log(result)
-}
 
 function App() {
+
+  const [foundUser, setFoundUser] = useState()
+
+  const searchUser = async (user) => {
+    const result = await axios.getAll(user)
+    setFoundUser(result)
+  }
+
+  
   return (
     <div className="App">
       Hello
       <Search searchUser={searchUser}/>
+      <Link to='/repos'><a >{foundUser ? foundUser.login : null}</a></Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Repos from './components/Repos/Repos'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="repos" element={<Repos />} />
+      </Routes>
+      
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
