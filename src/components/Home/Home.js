6165793f39dbb9ec0d7d2a0bd2d5a1daf24baca3
unknown <123dragaoo@gmail.com>
2022-05-03 10:27:07 -0300

    spinner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,15 +5,18 @@ import axios from '../../services/axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from '../../reducer/UserReducer'
 import UserCard from '../UserCard/UserCard';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 function Home() {
 
   const [messageClass, setMessageClass] = useState('error-message-container-disabled')
   const [cardClass, setCardClass] = useState('user-card-container-inactive')
-  
+  const [isLoading, setIsLoading] = useState(false)
+
   const dispatch = useDispatch()
 
   const searchUser = async (user) => {
+    setIsLoading(true);
     setCardClass('user-card-container-inactive')
     const result = await axios.getAll(user).catch(error => {
       setMessageClass('error-message-container-active')
@@ -26,11 +29,9 @@ function Home() {
     if(result) {
       dispatch(setUser(result))
       setCardClass('user_card-container')
+      setIsLoading(false)
     }   
   }
-
-  useEffect(() => {
-  }, [])
 
   return (
     <div className="App">
@@ -39,8 +40,9 @@ function Home() {
       <div className={messageClass}>
         <p>User not found.</p>
       </div>
-      
+      {isLoading ? <LoadingSpinner /> : null}
     </div>
+    
   );
 }
 
