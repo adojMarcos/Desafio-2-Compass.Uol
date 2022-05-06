13c693c10e372c08e1f7ed68174a12184dab4c8b
unknown <123dragaoo@gmail.com>
2022-05-06 13:11:34 -0300

    changed class to className on followerCard component

--- src/components/Followers/FollowerCard/FollowerCard.js
+++ src/components/Followers/FollowerCard/FollowerCard.js
@@ -4,7 +4,7 @@ const FollowerCard = ( { follower, index} ) => {
   return (
     <div key={follower.id} className="repo-card" style={{animationDelay: `${((index) * 0.1) % 3}s`}}>
         <img src={follower.avatar_url} alt="user avatar"  className="user_img" />
-        <p style={{fontSize: '2rem'}}><a class="anchor-reset" target="__blank" href={follower.html_url}>{follower.login}</a></p>
+        <p style={{fontSize: '2rem'}}><a className="anchor-reset" target="__blank" href={follower.html_url}>{follower.login}</a></p>
     </div>
   )
 }
